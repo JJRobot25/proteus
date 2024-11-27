22769a12f2fd8992193d45ad8fd8ec6d9061acba
JJRobot25 <126060044+JJRobot25@users.noreply.github.com>
2024-11-27 17:19:08 +0100

    Comments

--- _site/assets/js/navbar.js
+++ _site/assets/js/navbar.js
@@ -1,3 +1,4 @@
+// https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("topnav");
--- assets/js/topButton.js
+++ assets/js/topButton.js
@@ -1,3 +1,5 @@
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
